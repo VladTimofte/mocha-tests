7e0042ed28067b3d82c03c36151aef28619bfbfb
Vlad <vladtimofte999@gmail.com>
2024-09-04 19:22:12 +0300

    test

---
+++ src/script.js
@@ -0,0 +1,7 @@
+import Calculator from "./calculator.js";
+
+const calc = new Calculator()
+console.log('Addition:', calc.add(1, 2));
+console.log('Subtraction:', calc.subtract(5, 3));
+console.log('Multiplication:', calc.multiply(4, 2));
+console.log('Division:', calc.divide(10, 2));
