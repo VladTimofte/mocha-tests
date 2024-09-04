daf457ca4cad6cb48ce51696c775345b1cbb393e
Vlad <vladtimofte999@gmail.com>
2024-09-04 18:33:06 +0300

    test

--- test/examples.test.js
+++ test/examples.test.js
@@ -13,15 +13,29 @@ describe("Untest simplu de array", function () {
 // ===== ASSERT ===== //
 describe("Teste Assert", function () {
   describe("Numbere", function () {
+    
     //Numbere
     it("ar trebui sa verifice daca doua numere sunt egale", function () {
       assert.equal(5, 5, "5 este egal cu 5");
     });
     it("ar trebui sa verifice daca un numar este mai mare decat altul", function () {
-        assert.isAbove(10, 5, "10 este mai mare decat 5");
-      });
-      it("ar trebui sa verifice daca un numar este mai mic decat altul", function () {
-        assert.isBelow(4, 5, "4 este mai mic decat 5");
-      });
+      assert.isAbove(10, 5, "10 este mai mare decat 5");
+    });
+    it("ar trebui sa verifice daca un numar este mai mic decat altul", function () {
+      assert.isBelow(4, 5, "4 este mai mic decat 5");
+    });
   });
+
+  describe('Stringuri', function() {
+    //Stringuri
+    it('ar trebui sa verifice daca doua stringuri sunt egale', function() {
+        assert.equal('salut', 'salut', 'stringurile sunt egale')
+    })
+    it('ar trebui sa verifice daca un string continte un anumit substring', function() {
+        assert.include('salut Vlad', 'Vlad', 'sintrul contine cuvantul Vlad')
+    })
+    it('ar trebui sa verifice lingimea unui string', function() {
+        assert.lengthOf('chai', 4, 'lungimea string-ului este de 4')
+    })
+  })
 });
