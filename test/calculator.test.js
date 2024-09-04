eb2c0985b63368efb54dd6478f9de782eaeb920c
Elvis-Ganga <elvisganga@gmail.com>
2024-09-04 20:02:01 +0300

    Update calculator.test.js

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -39,4 +39,39 @@ describe("Calculator", () => {
       expect(rezult).to.not.be.a("string");
     });
   });
+
+  describe("Inmultire", () => {
+    it("Ar trebui inmulteasca 2 numere corect", () => {
+      const rezult = calculator.multiply(5, 3);
+      expect(rezult).to.equal(15);
+    });
+    it("ar trebui sa verifice ca un numar este mai mare decat altul", function () {
+      const rezult = calculator.multiply(5, 3);
+      expect(rezult).to.be.above(10);
+    });
+    it("ar trebui sa verifice ca un numar este mai mic decat altul", function () {
+      const rezult = calculator.multiply(5, 3);
+      expect(rezult).to.be.below(20);
+      expect(rezult).to.be.a("number");
+      expect(rezult).to.not.be.a("string");
+    });
+  });
+
+  describe("Impartire", () => {
+    it("Ar trebui imparta 2 numere corect", () => {
+      const rezult = calculator.divide(10, 2);
+      expect(rezult).to.equal(5);
+    });
+    it("ar trebui sa verifice ca un numar este mai mare decat altul", function () {
+      const rezult = calculator.divide(6, 3);
+      expect(rezult).to.be.above(1);
+    });
+    it("ar trebui sa verifice ca un numar este mai mic decat altul", function () {
+      const rezult = calculator.divide(10, 2);
+      expect(rezult).to.be.below(20);
+      expect(rezult).to.be.a("number");
+      expect(rezult).to.not.be.a("string");
+      expect(() => calculator.divide(10, 0)).to.throw("Cannot divide by zero");
+    });
+  });
 });
